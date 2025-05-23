ffd27cf6877c8bad9746370b5d6b19e3a6a0a48e
GrawikInGraw <166131364+GrawikInGraw@users.noreply.github.com>
2025-05-23 16:23:30 +0200

    Add files via upload

---
+++ User_Setup.h
@@ -0,0 +1,21 @@
+#define USER_SETUP_ID 32
+
+#define ST7735_DRIVER
+
+#define TFT_CS   16
+#define TFT_DC   2
+#define TFT_RST  4
+#define TFT_SCLK 21
+#define TFT_MOSI 22
+
+#define TFT_RGB_ORDER TFT_RGB
+
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
+#define LOAD_FONT6
+#define LOAD_FONT7
+#define LOAD_FONT8
+#define LOAD_GFXFF
+
+
